dd6a6db33af9d4cf293487235e8a970284cb81f4
Santosh C <128799305+Santosh-GS@users.noreply.github.com>
2024-06-07 11:05:53 +0530

    Create python_style_guide.py
    Added initial notes

---
+++ python_style_guide.py
@@ -0,0 +1,78 @@
+# Based on pep8 style guide : https://peps.python.org/pep-0008/
+
+# Naming Conventions and Various Cases
+'''
+SNAKE CASE:
+lower caps with semicolons
+used for general variables, functions, modules
+my_variable = 123
+def my_function() :
+main.py
+
+SCREAMING SNAKE CASE: (upper snake case)
+all upper caps with semicolons
+used for constant variables
+PI = 3.14
+ACCELERATION_DUE_TO_GRAVITY = 9.81
+SCREEN_HEIGHT, SCREEN_WIDTH = 1920, 1080
+
+PASCAL CASE: (Upper Camel Case)
+first capital letter for each word without any space
+used for classes and exceptions (exception is a class in python)
+class BaseClass
+class XException
+
+CAMEL CASE:
+pascal case with the very first letter in lower case
+rarely used in some python modules
+used to declare variable in other languages like java, cpp, etc.
+int myVariableName
+cv2.destroyAllWindows()
+
+KEBAB CASE: (lisp case, caterpillar case)
+snake case with dashes
+used while installing packages
+pip install opencv-python
+pip install pandas-datareader (but "import pandas_datareader" while importing)
+pip install -U scikit-learn
+
+FLAT CASE:
+all lower caps with no spaces
+not used, avoid
+myvariablename
+
+COBOL CASE:
+upper snake case with dashes
+not used usually
+MY-VARIABLE-NAME
+
+References:
+https://www.youtube.com/watch?v=D4_s3q038I0
+https://www.youtube.com/watch?v=uet8ZQpyJV8
+
+'''
+
+# General Guidelines
+'''
+* Limit each line to 80 characters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
